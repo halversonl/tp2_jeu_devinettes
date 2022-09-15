88fd6df44f5214314aa350ae1950580b0f500a77
halversonl <halversonleduc.college-montreal.qc.ca>
2022-09-15 10:06:03 -0400

    commit #3, ajout de la fonction de choisir les bornes, organisation du code, réparation du probleme ou on pouvait seulement jouer 2 fois de suite.

--- main.py
+++ main.py
@@ -2,29 +2,42 @@ import random
 
 def jeu():
 
-    #print("Choissisez la borne minimale et la borne maximale pour le jeu.")
-    nombre = random.randint(1,1000)
+    #Choix de bornes
+    print("Choissisez la borne minimale et la borne maximale pour le jeu.")
+
+    borne_minimale = int(input("Quel est votre borne minimale?: "))
+    borne_maximale = int(input("Quel est votre borne maximale?: "))
+
+    if borne_maximale < borne_minimale:
+        print("Vu que la borne minimale était plus grande que la borne maximale, on les a échangés.")
+        borne_maximale, borne_minimale = borne_minimale, borne_maximale
+
+    #Generation des variables
+    nombre = random.randint(borne_minimale,borne_maximale)
     nb_essais = 0
     essai = -1
 
-    print("J’ai choisi un nombre au hasard entre borne_minimale et borne_maximale. À vous de le deviner...")
-    #print(nombre)
+    #Début du jeu
+    print("J’ai choisi un nombre au hasard entre {} et {}. À vous de le deviner...".format(borne_minimale, borne_maximale))
+    print(nombre)
     while essai != nombre:
         essai = int(input("Entrer votre essai: "))
         nb_essais = nb_essais + 1
 
+        #Mauvaises réponses
         if essai > nombre:
             print("Mauvais choix, le nombre est plus petit que {}.".format(essai))
         elif essai < nombre:
             print("Mauvaise réponse, le nombre est plus grand que {}.".format(essai))
-
+    #Bonne réponse
     if essai == nombre:
         print("Bravo! Bonne réponse! Vous l'avez eu en {} essais.".format(nb_essais))
 
+    #Rejouer ou non
+    rejouer = input("Voulez-vous faire une autre partie (o/n)?")
+    if rejouer == "o":
+        jeu()
+    elif rejouer == "n":
+        print("Merci et au revoir…")
 jeu()
 
-rejouer = input("Voulez-vous faire une autre partie (o/n)?")
-if rejouer == "o":
-    jeu()
-elif rejouer == "n":
-    print("Merci et au revoir…")
