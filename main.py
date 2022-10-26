2222f1d438aafbd71e1b7dedecf6004aace5aeb6
halversonl <halversonleduc.college-montreal.qc.ca>
2022-10-26 08:26:05 -0400

    Commit #4 Ajout de deux docstrings

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+"""
+Crée par Leo Halverson le 23 septembre 2022
+Code qui génère un chiffre située entre les bornes déterminés par
+l'usager qui doit ensuite deviner ce chiffre.
+"""
+
 import random
 
-def jeu():
 
-    #Choix de bornes
+def jeu():
+    """
+    Fonction qui contient le jeu dans son entierté afin de rendre très facile
+    de le boucler.
+    :return:
+    """
+    # Choix de bornes
     print("Choissisez la borne minimale et la borne maximale pour le jeu.")
 
     borne_minimale = int(input("Quel est votre borne minimale?: "))
@@ -12,32 +23,34 @@ def jeu():
         print("Vu que la borne minimale était plus grande que la borne maximale, on les a échangés.")
         borne_maximale, borne_minimale = borne_minimale, borne_maximale
 
-    #Generation des variables
+    # Generation des variables
     nombre = random.randint(borne_minimale,borne_maximale)
     nb_essais = 0
     essai = -1
 
-    #Début du jeu
+    # Début du jeu
     print("J’ai choisi un nombre au hasard entre {} et {}. À vous de le deviner...".format(borne_minimale, borne_maximale))
-    print(nombre)
+    # print(nombre)
     while essai != nombre:
         essai = int(input("Entrer votre essai: "))
         nb_essais = nb_essais + 1
 
-        #Mauvaises réponses
+        # Mauvaises réponses
         if essai > nombre:
             print("Mauvais choix, le nombre est plus petit que {}.".format(essai))
         elif essai < nombre:
             print("Mauvaise réponse, le nombre est plus grand que {}.".format(essai))
-    #Bonne réponse
+    # Bonne réponse
     if essai == nombre:
         print("Bravo! Bonne réponse! Vous l'avez eu en {} essais.".format(nb_essais))
 
-    #Rejouer ou non
+    # Rejouer ou non
     rejouer = input("Voulez-vous faire une autre partie (o/n)?")
     if rejouer == "o":
         jeu()
     elif rejouer == "n":
         print("Merci et au revoir…")
+
+
 jeu()
 
